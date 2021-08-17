4d1bdc9effef96e380da811cddd45f93f842b4d3
Jakub Stiburek <alcolp96@gmail.com>
2021-08-17 13:35:11 +0200

    edited burger icon

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -13,6 +13,14 @@ const EmptyLine = styled.div`
   border-bottom: 5px solid ${({ theme }) => theme.miaRed};
 `
 
+const Arrow = styled.div`
+  width: 0;
+  height: 0;
+  border-left: 19px solid transparent;
+  border-right:  19px solid transparent;
+  border-top: 4.5px solid ${({ theme }) => theme.miaYellow};
+`
+
 const Burger = (props) => {
   if(!props.open){
     return (
@@ -28,6 +36,11 @@ const Burger = (props) => {
     return (
       <Wrapper>
         <Line/>
+        <EmptyLine/>
+        <Line/>
+        <EmptyLine/>
+        <Line/>
+        <Arrow/>
       </Wrapper>
     )
   }
