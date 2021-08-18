c21200cee06d5c9c77b24cee4b50f954da458aac
Jakub Stiburek <alcolp96@gmail.com>
2021-08-18 12:59:49 +0200

    refactored burger icon to svg icon

--- src/components/Burger.js
+++ src/components/Burger.js
@@ -1,46 +1,35 @@
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-  width: 38px;
-  margin: 40px auto;
+  width: max-content;
+  margin: 22.5px auto;
 `
 
-const Line = styled.div`
-  border-bottom: 5px solid ${({ theme }) => theme.miaYellow}
+const Icon = styled.svg`
+  width: 60px;
+  height: 60px;
+  fill: ${({ theme }) => theme.miaYellow};
 `
 
-const EmptyLine = styled.div`
-  border-bottom: 5px solid ${({ theme }) => theme.miaRed};
-`
-
-const Arrow = styled.div`
-  width: 0;
-  height: 0;
-  border-left: 19px solid transparent;
-  border-right:  19px solid transparent;
-  border-top: 4.5px solid ${({ theme }) => theme.miaYellow};
+const IconOpen = styled(Icon)`
+  fill: ${({ theme }) => theme.miaWhite};
 `
 
 const Burger = (props) => {
   if(!props.open){
     return (
       <Wrapper>
-        <Line/>
-        <EmptyLine/>
-        <Line/>
-        <EmptyLine/>
-        <Line/>
+        <Icon xmlns="http://www.w3.org/2000/svg" className="bi bi-list" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </Icon>
       </Wrapper>
     )
   } else {
     return (
       <Wrapper>
-        <Line/>
-        <EmptyLine/>
-        <Line/>
-        <EmptyLine/>
-        <Line/>
-        <Arrow/>
+        <IconOpen xmlns="http://www.w3.org/2000/svg" className="bi bi-list" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </IconOpen>
       </Wrapper>
     )
   }
