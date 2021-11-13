c2743b3daff3a39442138cdd7c3e1be356735c1e
jakub <alcolp96@gmail.com>
2021-11-13 20:21:26 +0100

    Background color fix

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import RoutedApp from './RoutedApp'
 import store from "./store";
 import {Provider} from "react-redux";
 import {QueryClient, QueryClientProvider} from "react-query";
-import {ChakraProvider} from "@chakra-ui/react";
+import {ChakraProvider, extendTheme} from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "#FFF4F5",
+      }
+    }
+  }
+})
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <Provider store={store}>
           <RoutedApp/>
         </Provider>
