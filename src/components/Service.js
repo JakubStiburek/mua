2c683faea4b9c928538c7539bf811f0dadc208a8
jakub <alcolp96@gmail.com>
2021-12-18 16:50:44 +0100

    Fix null props at services

--- src/components/Service.js
+++ src/components/Service.js
@@ -2,17 +2,13 @@ import {Box, Flex, Heading, Text} from "@chakra-ui/react";
 import ReactMarkdown from "react-markdown";
 
 const Service = ({product}) => {
-  const renderPrice = (product) => {
-    return product.priceMax ?
-      <Text fontSize="sm">{`${product.price} až ${product.priceMax} Kč`}</Text> :
-      <Text fontSize="sm">{`${product.price} Kč`}</Text>
-  }
-
   return (
     <Box w={270}>
       <Flex justify="space-between">
         <Heading size="sm">{product.title}</Heading>
-        {renderPrice(product)}
+        {product.priceMax ?
+          <Text fontSize="sm">{`${product.price} až ${product.priceMax} Kč`}</Text> :
+          product.price && <Text fontSize="sm">{`${product.price} Kč`}</Text>}
       </Flex>
       <ReactMarkdown>{product.description}</ReactMarkdown>
       <Box h={10}/>
